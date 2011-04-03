5007da5a960c5811cbfa14372a4939cc8abbdfad
Erik Peterson <cerikpete@gmail.com>
2011-04-03 15:50:52 -0400

    Added log4net and NHibernate.ByteCode dlls Added mapping test for the document domain object

---
+++ Data/Mapping/DocumentMap.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using FluentNHibernate.Mapping;
+using Data.Domain;
+
+namespace Data.Mapping
+{
+    public class DocumentMap : ClassMap<Document>
+    {
+        public DocumentMap()
+        {
+            Id(x => x.Id).Access.ReadOnlyPropertyThroughLowerCaseField(Prefix.Underscore);
+            Map(x => x.Name);
+        }
+    }
+}
---
+++ Tests/Mapping/DocumentMappingTests.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Tests.Mapping.BaseClasses;
+using Data.Domain;
+using NUnit.Framework;
+
+namespace Tests.Mapping
+{
+    [TestFixture]
+    public class When_mapping_a_Document : MappingTestFor<Document>    
+    {
+        [Test]
+        public void The_value_in_name_property_should_be_equal_to_the_value_in_the_name_column()
+        {
+            EnsureValueIn(x => x.Name).IsEqualToValueInColumnWithName("Name");
+        }
+
+        protected override string SqlToRetrieveTestDataRow
+        {
+            get { return string.Format("select * from Document where Id = '{0}'", systemUnderTest.Id); }
+        }
+    }
+}
